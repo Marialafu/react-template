0ef584bb106936e1fdfbe9b59c1fec2e0ba79797
Marialafu <lafuentelhmaria@gmail.com>
2025-05-07 09:36:51 +0200

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import Button from "./components/button/Button";
+import Button from './components/button/Button';
 
 const App = () => {
-  return  <>
-  <Button/>
-  <h1>React</h1>
-  </>
+  return (
+    <>
+      <Button />
+      <h1>React</h1>
+    </>
+  );
 };
 
 export default App;
